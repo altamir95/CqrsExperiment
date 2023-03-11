d2a34244f42f5d381dda1b360f3d8b854751bb63
altamir95 <altamir95@yandex.ru>
2023-03-11 16:38:20 +0300

    Fill

---
+++ Services/OrgManager/CQRS.OrgManager.WebApi/Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CQRS.OrgManager.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FirstController : ControllerBase
+{
+    private readonly ILogger<FirstController> _logger;
+
+    public FirstController(ILogger<FirstController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(true);
+    }
+}
+
